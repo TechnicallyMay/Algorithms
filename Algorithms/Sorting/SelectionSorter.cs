f2f9714d2ef3235a0225584dcd530d4166e445e3
Main <masonwells01@gmail.com>
2021-11-20 11:51:16 -0700

    Added selection sort algorithm

---
+++ Algorithms/Sorting/SelectionSorter.cs
@@ -0,0 +1,22 @@
+﻿namespace Algorithms.Sorting;
+
+internal class SelectionSorter : IInPlaceSorter<int>
+{
+    public void Sort(int[] items)
+    {
+        for (int i = 0; i < items.Length - 1; i++)
+        {
+            int smallestIndex = i;
+            for (int j = i + 1; j < items.Length; j++)
+            {
+                if (items[j] < items[smallestIndex])
+                    smallestIndex = j;
+            }
+
+            int copy = items[i];
+
+            items[i] = items[smallestIndex];
+            items[smallestIndex] = copy;
+        }
+    }
+}
