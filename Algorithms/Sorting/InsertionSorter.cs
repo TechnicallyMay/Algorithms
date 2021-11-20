166f401be8e60e0582bfec8c689238b35ebf39df
Main <masonwells01@gmail.com>
2021-11-20 12:54:25 -0700

    Added insertion sort algorithm

---
+++ Algorithms/Sorting/InsertionSorter.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms.Sorting;
+
+internal class InsertionSorter : IInPlaceSorter<int>
+{
+    public void Sort(int[] items)
+    {
+        for (int i = 1; i < items.Length; i++)
+        {
+            int j = i - 1;
+
+            while (j >= 0 && items[j + 1] < items[j])
+            {
+                int jCopy = items[j];
+                items[j] = items[j + 1];
+                items[j + 1] = jCopy;
+                --j;
+            }
+        }
+    }
+}
